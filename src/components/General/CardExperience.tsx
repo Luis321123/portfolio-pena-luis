e41ae847d4f0cc62ed3ededb85c87fef71f584ee
Luis321123 <luis1233210e@gmail.com>
2026-07-30 23:46:58 -0400

    go

--- src/components/General/CardExperience.tsx
+++ src/components/General/CardExperience.tsx
@@ -2,7 +2,7 @@ import { detail } from "@/services/Details";
 import { motion, useInView } from "framer-motion";
 import { useRef, useState } from "react";
 import TechBadge from "@/components/TechBadge";
-import TechIcon from "@/assets/icons/System.ico";
+import TechIcon from "@/assets/icons/sysstem.ico";
 import AchievementsIcon from "@/assets/icons/trophy.png";
 import DetailsIcon from "@/assets/icons/info.svg";
 
